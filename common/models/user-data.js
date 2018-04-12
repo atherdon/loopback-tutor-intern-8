88160f8ac8d6aeed3ce9f2583a03d395bdf4a1e7
chauhannishith <chauhannis123ith@gmail.com>
2018-04-12 16:41:18 +0530

    email partly works, add verification link in email

--- common/models/user-data.js
+++ common/models/user-data.js
@@ -3,41 +3,85 @@
 
 var config = require('../../server/config.json');
 var path = require('path');
-var senderEmailAddress = "arthur.tkachenko.netweight@gmail.com";
-var senderEmailPassword = "biBcf1K8r4Yn";
+var senderAddress = "arthur.tkachenko.netweight@gmail.com";
+//var senderEmailPassword = "biBcf1K8r4Yn";
 
 module.exports = function(Userdata) {
-
-	var User = app.models.userData;
-
-  User.create({email: 'foo@bar.com', password: 'bar'}, function(err, userInstance) {
-    console.log(userInstance);
-  });
-  
-  User.afterRemote('create', function(context, userInstance, next) {
-    console.log('==> user.afterRemote triggered');
-
+  //send verification email after registration
+  Userdata.afterRemote('create', function(context, user, next) {
+    console.log("this did execute");
     var options = {
       type: 'email',
-      to: userInstance.email,
-      from: senderEmailAddress,
+      to: user.email,
+      from: senderAddress,
       subject: 'Thanks for registering.',
+      text: "please verify the link",
       template: path.resolve(__dirname, '../../server/views/verify.ejs'),
       redirect: '/verified',
       user: user
     };
 
-    userInstance.verify(options, function(err, response, next) {
-      if (err) return next(err);
-
-      console.log('==> verification email sent:', response);
-
+    user.verify(options, function(err, response) {
+      if (err) {
+        Userdata.deleteById(user.id);
+        return next(err);
+      }
       context.res.render('response', {
         title: 'Signed up successfully',
-        content: 'Please check your email and click on the verification link before logging in.',
+        content: 'Please check your email and click on the verification link ' +
+            'before logging in.',
         redirectTo: '/',
         redirectToLinkText: 'Log in'
       });
+    });
+  });
+  
+  // Method to render
+  Userdata.afterRemote('prototype.verify', function(context, user, next) {
+    context.res.render('response', {
+      title: 'A Link to reverify your identity has been sent '+
+        'to your email successfully',
+      content: 'Please check your email and click on the verification link '+
+        'before logging in',
+      redirectTo: '/',
+      redirectToLinkText: 'Log in'
+    });
+  });
+
+  //send password reset link when requested
+  Userdata.on('resetPasswordRequest', function(info) {
+    var url = 'http://' + config.host + ':' + config.port + '/reset-password';
+    var html = 'Click <a href="' + url + '?access_token=' +
+        info.accessToken.id + '">here</a> to reset your password';
+
+    User.app.models.Email.send({
+      to: info.email,
+      from: senderAddress,
+      subject: 'Password reset',
+      html: html
+    }, function(err) {
+      if (err) return console.log('> error sending password reset email');
+      console.log('> sending password reset email to:', info.email);
+    });
+  });
+
+  //render UI page after password change
+  Userdata.afterRemote('changePassword', function(context, user, next) {
+    context.res.render('response', {
+      title: 'Password changed successfully',
+      content: 'Please login again with new password',
+      redirectTo: '/',
+      redirectToLinkText: 'Log in'
+    });
+  });
+
+  //render UI page after password reset
+  Userdata.afterRemote('setPassword', function(context, user, next) {
+    context.res.render('response', {
+      title: 'Password reset success',
+      content: 'Your password has been reset successfully',
+      redirectTo: '/',
+      redirectToLinkText: 'Log in'
     });
   });
 };
--- server/boot/routes.js
+++ server/boot/routes.js
@@ -1,52 +1,11 @@
 'use strict';
 
-
+var dsConfig = require('../datasources.json');
 module.exports = function(app) {
 
-const nodemailer = require('nodemailer');
-	var router = app.loopback.Router();
-
-	router.get('/hello', function(req, res,next) {
-		res.send("now lets check if this works");
-	});
-
-	app.use(router);
+	var User = app.models.userData;
 
 
-
-//==
-
-	router.post('/confirmmail', function(req, res, next){
-		let transporter = nodemailer.createTransport({
-			host: 'smtp.ethereal.email',
-			port: 587,
-			secure: false, // true for 465, false for other ports
-			auth: {
-				user: account.user, // generated email id
-				pass: account.pass // generated email password
-			}
-		});
-
-		// setup email data with unicode symbols
-		let mailOptions = {
-			from: '"Groceristar" <noreply@groceristar.com>', // send from address
-			to: 'bar@example.com, baz@example.com', // send to
-			subject: 'Welcome to Groceristar', // Subject line
-			text: 'Please click the link below to verify yourself', // plain text body
-			html: '<b>URL</b>' // html body
-		};
-
-// send mail with defined transport object
-		transporter.sendMail(mailOptions, (error, info) => {
-			if (error) {
-				return console.log(error);
-			}
-			console.log('Message sent: %s', info.messageId);
-			// Preview only available when sending through an Ethereal account
-			console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
-		// Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-		// Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-		});
-	});
+  
+  
 }
