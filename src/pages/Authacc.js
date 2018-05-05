7ac20076a03c892cd14f270d32fbccf57c613b40
chauhannishith <chauhannis123ith@gmail.com>
2018-05-05 15:37:36 +0530

    routes

--- src/pages/Authacc.js
+++ src/pages/Authacc.js
@@ -14,9 +14,9 @@ class Authacc extends Component{
 		axios.get(config.url + '/userstatus')
 		.then(response => {
 			if(response !== null){
-			console.log("This came->" + JSON.stringify(response))
+			console.log("This came->" + response)
 			this.setState({userdata: response.data})
-			console.log("state"+this.state.userdata.email)}
+			console.log("state"+this.state.userdata)}
 		}).catch(err => console.log(err))
 	}
 
